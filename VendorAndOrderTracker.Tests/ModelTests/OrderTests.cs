df309b449e06977433b49a179a871fa39ab83bc8
Richard <charichard09@gmail.com>
2022-12-16 09:43:04 -0800

    WIP: add tests for Order class

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -12,4 +12,14 @@ public class OrderTests
     Order newOrder = new Order("testName", "testDescription");
     Assert.AreEqual(typeof(Order), newOrder.GetType());
   }
+
+  [TestMethod]
+  public void GetDescriptionAndTitle_ReturnsDescriptionAndTitle_String()
+  {
+    string title = "Bread";
+    string description = "food made of flour, water, and yeast or another leavening agent, mixed together and baked.";
+    Order newOrder = new Order(title, description);
+    Assert.AreEqual(title, newOrder.Title);
+    Assert.AreEqual(description, newOrder.Description);
+  }
 }
