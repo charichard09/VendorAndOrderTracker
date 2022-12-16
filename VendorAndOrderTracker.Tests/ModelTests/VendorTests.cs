d7aff156fe71fe19ffb4f91a14c782f650a8c409
Richard <charichard09@gmail.com>
2022-12-16 10:24:25 -0800

    add all tests for Vendor

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -4,12 +4,88 @@ using VendorAndOrderTracker.Models;
 namespace VendorAndOrderTracker.Tests;
 
 [TestClass]
-public class VendorTests
+public class VendorTests : IDisposable
 {
+  public void Dispose()
+  {
+    Vendor.ClearAll();
+  }
+
   [TestMethod]
   public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
   {
     Vendor newVendor = new Vendor("testName", "testDescription");
     Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+  }
+
+  [TestMethod]
+  public void GetNameAndDescription_ReturnsNameAndDescription_String()
+  {
+    string name = "TestVendor";
+    string description = "TestDescription";
+    Vendor newVendor = new Vendor(name, description);
+
+    Assert.AreEqual(name, newVendor.Name);
+    Assert.AreEqual(description, newVendor.Description);
+  }
+
+  [TestMethod]
+  public void GetId_ReturnsVendorId_Int()
+  {
+    string name = "TestVendor";
+    string description = "TestDescription";
+    Vendor newVendor = new Vendor(name, description);
+
+    int result = newVendor.Id;
+
+    Assert.AreEqual(1, result);
+  }
+
+  [TestMethod]
+  public void GetAll_ReturnsAllVendorObjects_VendorList()
+  {
+    string name1 = "TestVendor1";
+    string description1 = "TestDescription1";
+    Vendor newVendor1 = new Vendor(name1, description1);
+    string name2 = "TestVendor2";
+    string description2 = "TestDescription2";
+    Vendor newVendor2 = new Vendor(name2, description2);
+    List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+    List<Vendor> result = Vendor.GetAll();
+
+    CollectionAssert.AreEqual(newList, result);
+  }
+
+  [TestMethod]
+  public void Find_ReturnsCorrectVendor_Vendor()
+  {
+    string name1 = "TestVendor1";
+    string description1 = "TestDescription1";
+    Vendor newVendor1 = new Vendor(name1, description1);
+    string name2 = "TestVendor2";
+    string description2 = "TestDescription2";
+    Vendor newVendor2 = new Vendor(name2, description2);
+
+    Vendor result = Vendor.Find(2);
+
+    Assert.AreEqual(newVendor2, result);
+  }
+
+  [TestMethod]
+  public void AddOrder_AssociatesOrderWithVendor_OrderList()
+  {
+    string title = "Bread";
+    string description = "food made of flour, water, and yeast or another leavening agent, mixed together and baked.";
+    Order newOrder = new Order(title, description);
+    List<Order> newList = new List<Order> { newOrder };
+    string name = "TestVendor";
+    string description = "TestDescription";
+    Vendor newVendor = new Vendor(name, description);
+    newVendor.AddOrder(newOrder);
+
+    List<Order> result = newVendor.Orders;
+
+    CollectionAssert.AreEqual(newList, result);
   }
 }
--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -4,10 +4,35 @@ public class Vendor
 {
   public string Name { get; set; }
   public string Description { get; set; }
+  public int Id { get; }
+  List<Vendor> _instances = new List<Vendor> {};
+  List<Order> Orders = new List<Order> {};
 
   public Vendor(string name, string description)
   {
     Name = name;
     Description = description;
+    _instances.Add(this);
+    Id = _instances.Count;
+  }
+
+  public static void ClearAll()
+  {
+    _instances.Clear();
+  }
+
+  public static List<Vendor> GetAll()
+  {
+    return _instances;
+  }
+
+  public static Vendor Find(int searchId)
+  {
+    return _instances[searchId-1];
+  }
+
+  public void AddOrder(Order order)
+  {
+    Orders.Add(order);
   }
 }
