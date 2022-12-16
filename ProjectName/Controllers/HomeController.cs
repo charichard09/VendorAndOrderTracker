31c1d91d79d303b68f6adba4c807973fb64e5d65
Richard <charichard09@gmail.com>
2022-12-16 08:33:36 -0800

    setup project folder

--- ProjectName/Controllers/HomeController.cs
+++ ProjectName/Controllers/HomeController.cs
@@ -6,32 +6,10 @@ namespace FriendLetter.Controllers
   public class HomeController : Controller
   {
 
-    [Route("/hello")]
-    public string Hello() { return "Hello friend!"; }
-
-    [Route("/goodbye")]
-    public string Goodbye() { return "Goodbye friend."; }
-
     [Route("/")]
-    public ActionResult Letter() 
+    public ActionResult Index() 
     {
-      LetterVariable myLetterVariable = new LetterVariable();
-      myLetterVariable.Recipient = "Lina";
-      myLetterVariable.Sender = "Jasmine";
-      return View(myLetterVariable);
+      return View();
     }
-
-    [Route("/form")]
-    public ActionResult Form() { return View(); }
-
-    [Route("/postcard")]
-    public ActionResult Postcard(string recipient, string sender)
-    {
-      LetterVariable myLetterVariables = new LetterVariable();
-      myLetterVariables.Recipient = recipient;
-      myLetterVariables.Sender = sender;
-      return View(myLetterVariables);
-    }
-
   }
 }
--- ProjectName/Program.cs
+++ ProjectName/Program.cs
@@ -13,6 +13,8 @@ namespace ProjectName
 
       WebApplication app = builder.Build();
 
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
       app.UseRouting();
 
       app.MapControllerRoute(
